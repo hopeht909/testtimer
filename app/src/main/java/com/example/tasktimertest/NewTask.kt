1daa8d04f610bbd245476a37695fd470ea703d7a
hopeht909 <77729536+hopeht909@users.noreply.github.com>
2021-11-03 11:12:29 +0300

    first

---
+++ app/src/main/java/com/example/tasktimertest/NewTask.kt
@@ -0,0 +1,11 @@
+package com.example.tasktimertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_task)
+    }
+}
---
+++ app/src/main/java/com/example/tasktimertest/ViewTasks.kt
@@ -0,0 +1,11 @@
+package com.example.tasktimertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_tasks)
+    }
+}
